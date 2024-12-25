539c46ac79583a7552d8e91a81b5e3166fc4f9b6
Muhammad Saadman Sakib Hossain Hossain <saadmansakib34@gmail.com>
2024-12-25 16:15:14 +0600

    Shaped using Nested loops

---
+++ Pattern_nested_loop/12345 12345 12345 12345.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+
+    int row,col,n;
+    printf("Enter N=");
+    scanf("%d",&n);
+    
+    for(row=1; row<=n; row++)
+    {
+        for(col=1; col<=n; col++)
+        {
+            printf("%d",col);
+        }
+printf("\n");
+    }
+}
---
+++ Pattern_nested_loop/Y shaped.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int row,n,col;
+    printf("enter value of n=");
+    scanf("%d",&n);
+    printf("\n\n");
+    for(row=1; row<n; row++)
+    {
+        for(col=1; col<n; col++)
+        {
+            if(row<=n/2 &&(row==col||row+col==n-1)||(row>n/2 && col==n/2))
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
